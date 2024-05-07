1fa5fbd070f4f9cd8328f5cd76fefe5ab00c3299
ChrisRoberts1 <croberts1101@gmail.com>
2024-05-07 11:16:09 -0400

    update pipeline

--- rtl/submodules/spm_channel.sv
+++ rtl/submodules/spm_channel.sv
@@ -9,28 +9,28 @@ module spm_channel #(
     input  wire                              rst_n,
 
     //inputs from decoder and BVB
-    input wire [SPM_ELE_W-1:0]               row_IDs_decoder_in     [CHAN_NUM-1:0],
-    input wire [SPM_ELE_W-1:0]               vector_values_BVB_in   [CHAN_NUM-1:0],
+    input wire [SPM_ELE_W-1:0]               row_IDs_decoder_in,
+    input wire [SPM_ELE_W-1:0]               vector_values_BVB_in,
     
     // inputs from arbiter
-    input wire  [SPM_ELE_W-1:0]              spm_val                [CHAN_NUM-1:0],
-    input wire  [SPM_ELE_W-1:0]              spm_row_len            [CHAN_NUM-1:0],
-    input wire  [SPM_ELE_W-1:0]              spm_col_idx            [CHAN_NUM-1:0],
+    input wire  [SPM_ELE_W-1:0]              spm_val,
+    input wire  [SPM_ELE_W-1:0]              spm_row_len,
+    input wire  [SPM_ELE_W-1:0]              spm_col_idx,
     input wire                               spm_fetch_stall, // Bubble signal if stall (don't push values)
     
     // Outputs 
-    output reg [SPM_ELE_W-1:0]               row_lengths_decoder_out [CHAN_NUM-1:0],
-    output reg [SPM_ELE_W-1:0]               row_IDs_accumulator_out [CHAN_NUM-1:0],
-    output reg [SPM_ELE_W-1:0]               mul_accumulator_out     [CHAN_NUM-1:0],
-    output reg [SPM_ELE_W-1:0]               col_IDs_BVB_out         [CHAN_NUM-1:0]
+    output reg [SPM_ELE_W-1:0]               row_lengths_decoder_out,
+    output reg [SPM_ELE_W-1:0]               row_IDs_accumulator_out,
+    output reg [SPM_ELE_W-1:0]               mul_accumulator_out,
+    output reg [SPM_ELE_W-1:0]               col_IDs_BVB_out
 );
 
-reg [SPM_ELE_W-1:0]               mul_0                 [CHAN_NUM-1:0];
-reg [SPM_ELE_W-1:0]               mul_1                 [CHAN_NUM-1:0];
-reg [SPM_ELE_W-1:0]               mul_2                 [CHAN_NUM-1:0];
-reg [SPM_ELE_W-1:0]               mul_3                 [CHAN_NUM-1:0];
-reg [SPM_ELE_W-1:0]               bvb_in_buffer         [CHAN_NUM-1:0];
-reg [SPM_ELE_W-1:0]               spm_val_buffer        [CHAN_NUM-1:0];
+reg [SPM_ELE_W-1:0]               mul_0;
+reg [SPM_ELE_W-1:0]               mul_1 ;
+reg [SPM_ELE_W-1:0]               mul_2;
+reg [SPM_ELE_W-1:0]               mul_3;
+reg [SPM_ELE_W-1:0]               bvb_in_buffer;
+reg [SPM_ELE_W-1:0]               spm_val_buffer;
 // fetch
 
 always @(posedge clk ) begin 
@@ -46,34 +46,26 @@ end
 integer i;
 always @(posedge clk ) begin 
     if (!spm_fetch_stall) begin
-        for (i=0; i< CHAN_NUM; i=i+1) begin
-            mul_0 [i] = spm_val_buffer[i] * bvb_in_buffer[i];
-        end
+        mul_0 = spm_val_buffer * bvb_in_buffer;
     end
 end
 
 // mul 1
 always @(posedge clk ) begin 
     if (!spm_fetch_stall) begin
-        for (i=0; i< CHAN_NUM; i=i+1) begin
-            mul_1 [i] = mul_0[i];
-        end
+        mul_0 = spm_val_buffer * bvb_in_buffer;
     end
 end
 // mul 2
 always @(posedge clk ) begin 
     if (!spm_fetch_stall) begin
-        for (i=0; i< CHAN_NUM; i=i+1) begin
-            mul_2 [i] = mul_1[i];
-        end
+        mul_0 = spm_val_buffer * bvb_in_buffer;
     end
 end
 // mul 3
 always @(posedge clk ) begin 
     if (!spm_fetch_stall) begin
-        for (i=0; i< CHAN_NUM; i=i+1) begin
-            mul_3 [i] = mul_2[i];
-        end
+        mul_0 = spm_val_buffer * bvb_in_buffer;
     end
 end
 // To Accumulator
